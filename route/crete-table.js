bdcf118038d859abe09e80257e0f08aec0b21c9f
khvvk0105 <147786228+khvvk0105@users.noreply.github.com>
2024-03-12 23:02:46 +0800

    login table

--- route/crete-table.js
+++ route/crete-table.js
@@ -1,25 +1,28 @@
 const { Pool } = require("pg");
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, PGPORT } = process.env;
 const pool = new Pool({
   host: PGHOST,
   database: PGDATABASE,
   username: PGUSER,
   password: PGPASSWORD,
-  port: 5432,
+  port: PGPORT,
   ssl: {
     require: true,
   },
 });
+// console.log("pool", pool);
 exports.createTable = async (req, res) => {
+  console.log("heviiin");
   const client = await pool.connect();
+  const Query = `CREATE TABLE login (name VARCHAR(255), password VARCHAR(255))`;
   try {
-    client.query(`CREATE TABLE login AS
-  SELECT name VARCHAR(255),password VARCHAR(255),FROM users WHERE`);
+    await client.query(Query);
+    console.log("try ajiljiin");
+    res.status(200).send({ message: "success" });
   } catch (error) {
     console.log(error);
   } finally {
     client.release();
   }
-  res.status(200).send({ message: "success" });
 };
--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { addUser } = require("./route/add-user");
 const { getUser } = require("./route/get-user");
-const { createTable } = require("./route/table");
+const { createTable } = require("./route/crete-table");
 const app = express();
 
 const router = express.Router();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(router);
 
 router.post("/add-user", addUser);
 router.get("/get-user", getUser);
-router.post("/login", createTable);
+router.post("/crete-table", createTable);
 
-app.use(router);
-
-app.listen(3001, () => {
-  console.log("Server is listening at port 3001");
+app.listen(4000, () => {
+  console.log("Server is listening at port 4000");
 });
